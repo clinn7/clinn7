e5f3570a5a36d9bdafd5e59989076f40519c095d
clinn7 <104385611+clinn7@users.noreply.github.com>
2022-04-25 17:36:18 -0500

    Update webformScript.py

--- webformScript.py
+++ webformScript.py
@@ -1,7 +1,7 @@
 #This the very first program I created, trimmed to be edited for any usecase. 
 #The main function is to repeatedly fill out 2 webforms on a timer.
 #A bit messy, but you'd be surprised how much cleaner it is from the original version.
-#chevrons<> and text within must be customized your info
+#chevrons<> and text within must be customized with your info
 
 webFormField1 = "<text to be entered into first field>"
 webFormField2 = "<text to be entered into second field>"
